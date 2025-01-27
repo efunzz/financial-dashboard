b2520f89f33e170cfd916e4972d1d6e86bb9f340
efunzz <irfansofyan2001@gmail.com>
2025-01-27 13:43:48 +0800

    sidebar-mobile-V1 (hamburger issue)
    -in large screen , hamburger can still be seen at the bottom even though set to hidden.

--- financial-dashboard/src/app/layout/sidebar/sidebar.component.ts
+++ financial-dashboard/src/app/layout/sidebar/sidebar.component.ts
@@ -3,31 +3,68 @@ import { SidebarService } from '../service/sidebar.service';
 import { MenuItem } from '../service/menu-item';
 import { ProfileComponent } from '../profile/profile.component';
 import { NavigationComponent } from '../navigation/navigation.component';
-
+import { DrawerModule } from 'primeng/drawer';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [ProfileComponent, NavigationComponent],
-  template:`
-      <!-- Update to use p-drawer -->
-      <aside class="h-screen w-64 bg-off-white text-black flex flex-col p-4 gap-y-64">
-        <div> 
+  standalone: true,
+  imports: [
+    ProfileComponent, 
+    NavigationComponent,
+    DrawerModule,
+    ButtonModule
+  ],
+  template: `
+    <!-- Desktop Sidebar -->
+    <aside class="hidden lg:flex h-screen w-64 bg-off-white text-black flex-col p-4 gap-y-60">
+      <div>
+        <h2 class="text-5xl font-bold ml-4">HDB</h2>
+      </div>
+      <app-navigation [menuItems]="menuItems"></app-navigation>
+      <app-profile></app-profile>
+    </aside>
+
+    <!-- Mobile Drawer -->
+    <p-drawer [(visible)]="drawerVisible" 
+              [showCloseIcon]="false"
+              position="left"
+              styleClass="custom-drawer"
+              [style]="{'background-color': '#F5F5F5'}"
+              >
+              <!-- Custom Close Icon -->
+            <button pButton 
+                    icon="pi pi-times" 
+                    (click)="drawerVisible = false"
+                    style="position: absolute; top: 20px; right: 20px; z-index: 999;" 
+                    class="p-button-text"></button>
+      <div class="flex flex-col p-4 gap-y-64">
+        <div>
           <h2 class="text-5xl font-bold ml-4">HDB</h2>
         </div>
-        <!-- Navigation Section -->
         <app-navigation [menuItems]="menuItems"></app-navigation>
-        <!-- Profile Section -->
-         <app-profile></app-profile>
-      </aside>
+        <app-profile></app-profile>
+      </div>
+    </p-drawer>
+
+    <!-- Hamburger button -->
+    <button pButton 
+            icon="pi pi-bars" 
+            (click)="toggleDrawer()"
+            class="p-button-text lg:hidden top-4 left-4 z-50">
+    </button>
   `,
-  styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
   menuItems: MenuItem[] = [];
-  sidebarService = inject (SidebarService);
+  drawerVisible = false;
+  sidebarService = inject(SidebarService);
 
-  constructor (){
+  constructor() {
     this.menuItems = this.sidebarService.getMenuItems();
   }
 
+  toggleDrawer() {
+    this.drawerVisible = !this.drawerVisible;
+  }
 }
